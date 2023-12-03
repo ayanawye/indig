ff287895fc6f4998e03671328b63b5c6e0358d76
My Name <kaichybekovaayana@gmail.com>
2023-12-03 11:59:53 +0600

    prepered for work

---
+++ src/components/Contribution/Contribution.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Contribution.scss";
+
+const Contribution = () => {
+  return <div></div>;
+};
+
+export default Contribution;
---
+++ src/components/ContributionGraph/ContributionGraph.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ContributionGraph.scss";
+
+const ContributionGraph = () => {
+  return <div>графа</div>;
+};
+
+export default ContributionGraph;
